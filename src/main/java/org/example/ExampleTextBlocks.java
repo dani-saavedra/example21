72a32fc9d5b7c18c3ca78399ba2a87233c826e6f
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2024-11-19 20:34:43 -0500

    Example TextBlocks

---
+++ src/main/java/org/example/ExampleTextBlocks.java
@@ -0,0 +1,46 @@
+package org.example;
+
+/*
+    Los Text Blocks, introducidos en Java 15,
+    son una característica que permite manejar cadenas de texto multilínea de manera más legible y limpia.
+    Se utilizan para evitar la complejidad de manejar múltiples concatenaciones o caracteres de escape cuando trabajamos con textos largos,
+    como consultas SQL o un JSON.
+ */
+public class ExampleTextBlocks {
+    public String basic() {
+        return """
+                Line 1: This is a tab: \t
+                Line 2: This is a quote: \"
+                Line 3: This is a backslash: \\
+                """;
+
+
+    }
+
+    public String query() {
+        return """
+                SELECT id, name, email
+                FROM users
+                WHERE status = 'active'
+                ORDER BY name;
+                """;
+    }
+
+    public String json() {
+        return """
+                {
+                    "name": "Alice",
+                    "age": 30,
+                    "isActive": true
+                }
+                """;
+    }
+
+    public String antes() {
+        return "{\n" +
+                "    \"name\": \"Alice\",\n" +
+                "    \"age\": 30,\n" +
+                "    \"isActive\": true\n" +
+                "}";
+    }
+}
